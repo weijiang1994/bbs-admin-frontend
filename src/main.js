25c708286dfa205963910095477efd5506e14a16
jiangwei1 <jiangwei1@kylinos.cn>
2021-12-01 10:26:01 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+// import Button from 'ant-design-vue/lib/button'
+import {Button} from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 import App from './App.vue'
+import router from './router'
+Vue.component(Button.name, Button)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
