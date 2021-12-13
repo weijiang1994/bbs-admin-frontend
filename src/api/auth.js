97c03a09aae1327a92575d3f223fbd72a13281f6
jiangwei1 <jiangwei1@kylinos.cn>
2021-12-13 17:26:30 +0800

    auth认证后端请求封装为api

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,30 @@
-import axios from "@/api/base.js";
+import request from '@/util/request'
 
 
-axios.get(
-    
-)
+function login(params){
+    return request({
+        url: '/auth/login',
+        method: 'post',
+        data: params
+    }
+    )
+}
+
+
+function logout(params){
+    return request({
+        url: '/auth/logout',
+        method: 'post',
+        data: params
+    })
+}
+
+function getUserInfo(params){
+    return request({
+        url: '/auth/userInfo',
+        method: 'get',
+        data: params
+    })
+}
+
+export {login, logout, getUserInfo}
--- src/api/base.js
+++
@@ -1,8 +0,0 @@
-import { Axios } from "axios";
-
-
-
-
-const axios = new Axios({
-    baseURL: process.env.BASE_BACEEND_URL
-})
