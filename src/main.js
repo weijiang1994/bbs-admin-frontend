807b9540208a607862e377ed46756bdc6a91bb10
jiangwei1 <jiangwei1@kylinos.cn>
2021-12-13 17:27:16 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ import VueAxios from 'vue-axios'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.use(VueAxios, axios)
-axios.defaults.baseURL = process.env.VUE_APP_BASE_BACKEND_URL;
-axios.defaults.headers['Content-Type'] = "application/json"
 
 new Vue({
   router,
