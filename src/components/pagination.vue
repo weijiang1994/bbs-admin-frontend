9a51ae491f58eee75b8fc6c1dd6c0def4c667cf4
weijiang1994 <804022023@qq.com>
2021-12-15 10:48:49 +0800

    增加分页组件

---
+++ src/components/pagination.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="block">
+    <el-pagination
+      background
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    },
+  },
+  data() {
+    return {
+      currentPage: 1,
+      pageSize: 20,
+    };
+  },
+};
+</script>
