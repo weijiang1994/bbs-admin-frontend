50fa9b2efa7e98576dc0966f6f7b401486f56311
weijiang1994 <804022023@qq.com>
2021-12-17 11:15:52 +0800

    增加全屏显示功能按钮

--- src/components/nav-top.vue
+++ src/components/nav-top.vue
@@ -5,21 +5,17 @@
     </el-aside>
     <el-aside width="auto" class="header-logo tap">
       <div class="mr-2">
-        <el-tooltip
-          effect="dark"
-          content="论坛前台页面"
-          placement="bottom"
-        >
-          <el-button
-            size="mini"
-            circle
-            @click="goFrontend"
-          ><i class="fa fa-home"></i></el-button>
+        <el-tooltip effect="dark" content="论坛前台页面" placement="bottom">
+          <el-button size="mini" circle @click="goFrontend"
+            ><i class="fa fa-home"></i
+          ></el-button>
         </el-tooltip>
       </div>
       <div class="mr-2">
         <el-tooltip effect="dark" content="后台主页面" placement="bottom">
-          <el-button @click="goDashborad" size="mini" circle><i class="fa fa-tachometer"></i></el-button>
+          <el-button @click="goDashborad" size="mini" circle
+            ><i class="fa fa-tachometer"></i
+          ></el-button>
         </el-tooltip>
       </div>
 
@@ -32,10 +28,21 @@
       <div class="mr-2">
         <el-tooltip effect="dark" content="查看消息" placement="top">
           <el-badge is-dot class="item">
-            <el-button size="mini" circle><i class="fa fa-bell-o"></i></el-button>
+            <el-button size="mini" circle
+              ><i class="fa fa-bell-o"></i
+            ></el-button>
           </el-badge>
         </el-tooltip>
       </div>
+
+      <div class="mr-2">
+        <el-tooltip effect="dark" content="全屏显示" placement="top">
+          <el-button size="mini" circle @click="fullScreen"
+            ><i class="fa fa-arrows-alt"></i
+          ></el-button>
+        </el-tooltip>
+      </div>
+
       <el-dropdown @command="handleCommand">
         <span class="el-dropdown-link">
           <el-avatar :src="avatar"></el-avatar>
@@ -56,20 +63,47 @@
 <script>
 import { removeToken } from "@/util/token";
 import { getUserInfo } from "@/api/auth";
+
 export default {
   data() {
     return {
       activeIndex: "1",
       avatar: "",
       userId: undefined,
+      fullscreen: false,
     };
   },
   methods: {
-    goDashborad(){
-      this.$router.push('/index')
+    fullScreen() {
+      let element = document.documentElement;
+      if (this.fullscreen) {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.webkitCancelFullScreen) {
+          document.webkitCancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      } else {
+        if (element.requestFullscreen) {
+          element.requestFullscreen();
+        } else if (element.webkitRequestFullScreen) {
+          element.webkitRequestFullScreen();
+        } else if (element.mozRequestFullScreen) {
+          element.mozRequestFullScreen();
+        } else if (element.msRequestFullscreen) {
+          // IE11
+          element.msRequestFullscreen();
+        }
+      }
+      this.fullscreen = !this.fullscreen;
+    },
+    goDashborad() {
+      this.$router.push("/index");
     },
     goFrontend() {
-      console.log(process.env);
       window.open(process.env.VUE_APP_FRONTEND_HOME);
     },
     handleSelect(key, keyPath) {
