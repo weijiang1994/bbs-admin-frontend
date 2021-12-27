c0a114e4161067a26faddc1497a38d8802b75f75
weijiang1994 <804022023@qq.com>
2021-12-27 14:54:26 +0800

    添加首页社区信息卡片以及自定义卡片组件

---
+++ src/components/CommunityCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <el-skeleton animated :loading="loading">
+      <template slot="template">
+        <div class="d-flex">
+          <el-skeleton-item variant="p mr-2"></el-skeleton-item>
+          <el-skeleton-item variant="p"></el-skeleton-item>
+        </div>
+        <el-skeleton-item variant="p mt-2"></el-skeleton-item>
+        <el-skeleton-item variant="p"></el-skeleton-item>
+      </template>
+    </el-skeleton>
+    <template>
+      <div class="community-card">
+        <div class="d-flex">
+          <p>{{ infos.title }}</p>
+          <div style="flex-grow: 1; display: flex; flex-direction: row-reverse">
+            <p class="badge-info">实时</p>
+          </div>
+        </div>
+        <p class="count-detail">{{ infos.count }}</p>
+        <p class="community-rooter">{{ infos.rooter }}</p>
+      </div>
+    </template>
+  </div>
+</template>
+<script>
+import request from "@/util/request";
+export default {
+  name: "CommunityCard",
+  data() {
+    return {
+      loading: true,
+      infos: {
+        title: "",
+        count: "",
+        rooter: "",
+      },
+    };
+  },
+  props: {
+    apiUrl: { type: String, default: "/community/user" },
+  },
+  created() {
+    request({
+      url: this.apiUrl,
+      method: "get",
+    })
+      .then((res) => {
+        this.infos = res.data;
+        this.loading = false;
+      })
+      .catch(() => {});
+  },
+};
+</script>
+<style scoped>
+.community-card {
+  background: #f1effd;
+  padding: 15px;
+  border-radius: 5px;
+  font-size: 12px;
+}
+.d-flex {
+  display: flex;
+}
+.count-detail {
+  font-size: 28px;
+  font-weight: bold;
+}
+.badge-info-div {
+  flex-grow: 1;
+  text-align: end;
+}
+.badge-info {
+  border-radius: 2px;
+  background: #1e9fff;
+  font-size: 12px;
+  padding: 1px;
+  color: white;
+}
+</style>
--- src/views/centre.vue
+++ src/views/centre.vue
@@ -2,12 +2,58 @@
 </style>
 <template>
   <div id="centre">
+    <el-row :gutter="10">
+      <el-col :span="18">
+        <el-row :gutter="10">
+          <el-col :span="12">
+            <el-card shadow="hover">
+              <div slot="header">
+                <i class="fa fa-cube mr-3"></i>社区概况
+              </div>
+              <el-row :gutter="10">
+                <el-col :span="12">
+                  <community-card
+                    apiUrl="/community/user/info"
+                  ></community-card>
+                </el-col>
+                <el-col :span="12">
+                  <community-card
+                    apiUrl="/community/post/info"
+                  ></community-card>
+                </el-col>
+              </el-row>
+              <el-row :gutter="10" class="mt-10">
+                <el-col :span="12">
+                  <community-card apiUrl="/community/comment/info"></community-card>
+                </el-col>
+                <el-col :span="12"><community-card apiUrl="/community/user/register"></community-card></el-col>
+              </el-row>
+            </el-card>
+          </el-col>
+          <el-col :span="12">
+            <el-card shadow="hover">
+              <div slot="header"><i class="fa fa-map-marker mr-2"></i> 快捷入口</div>
+              <div></div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="6"></el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import CommunityCard from "@/components/CommunityCard";
 export default {
-  name: "centre",
-  components: {},
+  name: "Home",
+  components: {
+    CommunityCard
+  },
 };
 </script>
+<style scoped>
+/deep/ .el-card__header {
+  padding: 8px;
+}
+</style>
