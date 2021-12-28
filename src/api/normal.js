a22c5b94ea996ae37b6ace3417c0cac5120aa0ce
weijiang1994 <804022023@qq.com>
2021-12-28 18:02:58 +0800

    后台主页相关功能

---
+++ src/api/normal.js
@@ -0,0 +1,22 @@
+import request from '@/util/request'
+
+
+function latestAdminLog() {
+    return request({
+        url: '/community/latest/admin-log',
+        method: 'get'
+    })
+}
+
+
+function serverStatus() {
+    return request({
+        url: '/community/server-status',
+        method: 'get'
+    })
+}
+
+export {
+    latestAdminLog,
+    serverStatus
+}
