9e9cf95d641a8b74056f113534f0e153706ca544
weijiang1994 <804022023@qq.com>
2022-01-10 17:57:18 +0800

    添加首页折线图(目前还是示例数据)

---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,116 @@
+<template>
+  <div :id="id" style="width: 100%;height: 300px"></div>
+</template>
+<script>
+import echarts from "echarts";
+import chartTheme from "@/components/charts/theme/westeros.json";
+export default {
+  name: "LineChart",
+  data() {
+    return {
+      id: "lineChart",
+      chart: null,
+      option: {},
+    };
+  },
+  mounted() {
+    this.initChart();
+  },
+  methods: {
+    initChart() {
+      echarts.registerTheme("westeros", chartTheme);
+      this.chart = echarts.init(document.getElementById(this.id), "westeros");
+      this.chart.setOption(this.loadOption());
+    },
+    loadOption() {
+      let data = {
+        title: {
+          text: "",
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#6a7985",
+            },
+          },
+        },
+        legend: {
+          data: ["访问量", "基金", "债券", "储蓄", "期货"],
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            type: "category",
+            boundaryGap: false,
+            data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+          },
+        ],
+        yAxis: [
+          {
+            type: "value",
+          },
+        ],
+        series: [
+          {
+            name: "访问量",
+            type: "line",
+            stack: "总量",
+            areaStyle: { normal: {} },
+            data: [120, 132, 101, 134, 90, 230, 210],
+          },
+          {
+            name: "基金",
+            type: "line",
+            stack: "总量",
+            areaStyle: { normal: {} },
+            data: [220, 182, 191, 234, 290, 330, 310],
+          },
+          {
+            name: "债券",
+            type: "line",
+            stack: "总量",
+            areaStyle: { normal: {} },
+            data: [150, 232, 201, 154, 190, 330, 410],
+          },
+          {
+            name: "期货",
+            type: "line",
+            stack: "总量",
+            areaStyle: { normal: {} },
+            data: [320, 332, 301, 334, 390, 330, 320],
+          },
+          {
+            name: "储蓄",
+            type: "line",
+            stack: "总量",
+            label: {
+              normal: {
+                show: true,
+                position: "top",
+              },
+            },
+            areaStyle: { normal: {} },
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+          },
+        ],
+      };
+      return data;
+    },
+  },
+  // beforeDestroy() {
+  //   // 销毁表格
+  //   if (!this.chart) {
+  //     return;
+  //   }
+  //   this.chart.dispose();
+  //   this.chart = null;
+  // },
+};
+</script>
